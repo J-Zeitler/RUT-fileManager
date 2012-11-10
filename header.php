a2029bc74aed9568b9d08cafdcaca1b034ff1151
Jonas Zeitler <jonasemanuelzeitler@gmail.com>
2012-11-11 00:05:24 +0100

    added more file structure

---
+++ header.php
@@ -0,0 +1,7 @@
+<header id="main-header">
+
+	<section id="header-inset">
+		<h1>RUTs filbibliotek</h1>
+	</section>
+
+</header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>RUTs Filbibliotek</title>
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <!--<script src="js/vendor/modernizr-2.6.2.min.js"></script>-->
+    </head>
+    <body>
+        <?php include('header.php'); ?>
+
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <?php include('page.php'); ?>
+    </body>
+</html>
