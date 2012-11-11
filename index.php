cacd05e3c3cbdc4a9a2f5e9fae397eb826263e7c
Jonas Zeitler <jonasemanuelzeitler@gmail.com>
2012-11-11 22:25:31 +0100

    added mvc structure

--- index.php
+++ index.php
@@ -6,17 +6,17 @@
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="view/css/normalize.css">
+        <link rel="stylesheet" href="view/css/main.css">
         <!--<script src="js/vendor/modernizr-2.6.2.min.js"></script>-->
     </head>
     <body>
-        <?php include('header.php'); ?>
+        <?php include('view/templates/header.php'); ?>
 
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <?php include('page.php'); ?>
+        <?php include('view/templates/page.php'); ?>
     </body>
 </html>
